aff12c531386a564ad0fa1226cab4055996b5bec
user140056 <user140056@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-05-06 12:14:07 -0700

    Colocando Decimal pad em tf de cotação e IOF

--- EvertonTeotonio/StateViewController.swift
+++ EvertonTeotonio/StateViewController.swift
@@ -40,7 +40,6 @@ class StateViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
